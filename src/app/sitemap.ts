b25f90e6991a3710e1f850fe23ba16095527dce2
nandeeshlaxetti-prog <nandeeshlaxetti@gmail.com>
2025-09-24 23:24:54 +0530

    Fix sitemap.ts build error by removing dynamic blogPosts import

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,4 @@
 import type { MetadataRoute } from 'next';
-import { blogPosts } from './blogs/page';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = 'https://lnnlegal.in';
@@ -9,8 +8,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '', 'about', 'services', 'team', 'blogs', 'contact'
   ];
   
-  // Blog pages
-  const blogPages = blogPosts.map(post => `/blogs/${post.id}`);
+  // Blog pages - using static IDs since we removed the dynamic import
+  const blogPages = [
+    '/blogs/7', // SARFAESI Act
+    '/blogs/8', // Consumer Complaints
+    '/blogs/9', // Real Estate Law
+    '/blogs/10', // Family Law
+    '/blogs/11', // Criminal Defense
+    '/blogs/12', // Corporate Law
+    '/blogs/13', // Banking Law
+    '/blogs/14', // IP Law
+    '/blogs/15', // Labor Law
+    '/blogs/16', // Services Law
+  ];
   
   // Combine all pages
   const allPages = [...mainPages, ...blogPages];
